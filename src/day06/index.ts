5d8a07a55de1ccad1670c38053e833f39332b9c0
Steven Hobson-Campbell <xapphire13@live.com>
2019-12-06 18:04:55 -0800

    Day 6 Part 2

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -62,4 +62,47 @@ async function readInput(): Promise<Map<string, string>> {
   console.log(`${chalk.bold("Part 1:")} ${chalk.yellow(calculateTotalOrbits())}`);
 
   // ===== Part 2 =====
+  function seek(itemName: string, distance: number) {
+    let newItem: string | undefined = itemName;
+    for (let i = 0; i < distance && newItem; i++) {
+      newItem = directOrbits.get(newItem);
+    }
+
+    return newItem;
+  }
+  function calculateDistance(from: string | undefined, to: string | undefined) {
+    let distance = 0;
+
+    while (from && from !== to) {
+      from = seek(from, 1);
+      distance++;
+    }
+
+    return distance;
+  }
+  function findIntersection(item1: string | undefined, item2: string | undefined) {
+    while (item1 && item2 && item1 !== item2) {
+      item1 = seek(item1, 1);
+      item2 = seek(item2, 1);
+    }
+
+    return item1 === item2 ? item1 : undefined;
+  }
+
+  const ourStartingPosition = "YOU";
+  const santasStartingPosition = "SAN";
+  let ourPosition: string | undefined = ourStartingPosition;
+  let santasPositon: string | undefined = santasStartingPosition;
+  const ourDistanceFromCom = calculateDistance(ourPosition, "COM");
+  const santasDistanceFromCom = calculateDistance(santasPositon, "COM");
+  if (ourDistanceFromCom > santasDistanceFromCom) {
+    ourPosition = seek(ourPosition, ourDistanceFromCom - santasDistanceFromCom);
+  } else if (santasDistanceFromCom > ourDistanceFromCom) {
+    santasPositon = seek(santasPositon, santasDistanceFromCom - ourDistanceFromCom);
+  }
+  const intersection = findIntersection(ourPosition, santasPositon);
+  // We need to subtract 2 to account for the fact distance is from the given item, not the item it's orbiting
+  const transfersRequired = calculateDistance(ourStartingPosition, intersection) + calculateDistance(santasStartingPosition, intersection) - 2;
+
+  console.log(`${chalk.bold("Part 2:")} ${chalk.yellow(transfersRequired)}`);
 })();
