7bb5f9c300ee24ff480a8a36f6dd0cf62adae6c7
Steven Hobson-Campbell <xapphire13@live.com>
2019-12-11 00:05:43 -0800

    Day 9 Part 1

--- src/day09/index.ts
+++ src/day09/index.ts
@@ -2,14 +2,17 @@
 import path from "path";
 import getQuestionSrcDir from "../utils/getQuestionSrcDir";
 import chalk from "chalk";
+import ShipComputer from "../ShipComputer";
 
 const INPUT_PATH = path.join(getQuestionSrcDir(9), "input.txt");
 
 (async () => {
   console.log(chalk.bold.white("===== Day 9 ====="));
+  const program = await ShipComputer.loadProgram(INPUT_PATH);
 
   // ===== Part 1 =====
-  console.log(`${chalk.bold("Part 1:")} ${chalk.yellow("TOD")}`);
+  const output1 = await ShipComputer.executeProgram(program.slice(), { input: [1] });
+  console.log(`${chalk.bold("Part 1:")} ${chalk.yellow(output1)}`);
 
   // ===== Part 2 =====
   console.log(`${chalk.bold("Part 2:")} ${chalk.yellow("TODO")}`);
