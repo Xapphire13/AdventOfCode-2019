22f26d1e4e9b2be4492719c5672e8f6c39ae1c22
Steven Hobson-Campbell <xapphire13@live.com>
2019-12-10 21:40:03 -0800

    fix day 05

--- src/day05/index.ts
+++ src/day05/index.ts
@@ -13,11 +13,11 @@ const INPUT_PATH = path.resolve(getQuestionSrcDir(5), "input.txt");
 
   // ===== Part 1 =====
   const program1 = program.slice();
-  console.log(`${chalk.bold("Part 1:")}`);
-  await ShipComputer.executeProgram(program1, { input: [1] });
+  const res1 = await ShipComputer.executeProgram(program1, { input: [1] });
+  console.log(`${chalk.bold("Part 1:")} ${chalk.yellow(res1[res1.length - 1])}`);
 
   // ===== Part 2 =====
   const program2 = program.slice();
-  console.log(`${chalk.bold("Part 2:")}`);
-  await ShipComputer.executeProgram(program2, { input: [5] });
+  const res2 = await ShipComputer.executeProgram(program2, { input: [5] });
+  console.log(`${chalk.bold("Part 2:")} ${chalk.yellow(res2[0])}`);
 })()
